348954372989b4ec2a8f1cbfdd00a235727935fb
Athena <athena.partch@gmail.com>
2021-02-20 08:01:57 -0800

    add listener

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World')
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 const es6Renderer = require('express-es6-template-engine');
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
-const server = http.createServer(app);
-const db = require('./db');
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// const server = http.createServer(app);
+// const db = require('./db');
